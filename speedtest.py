462ac10e109b158482060889a0389e3e4257a88f
Julien Kindle <jkindle@student.ethz.ch>
2020-02-10 17:15:22 +0100

    Improvements

--- speedtest.py
+++ speedtest.py
@@ -1,26 +1,37 @@
 from stable_baselines import PPO2
 import sys
 import time
+from multiprocessing import Pool
+import numpy as np
 
 
-exec_time = int(sys.argv[2])
+# Parse inputs
+model_path = sys.argv[1]
+secs = int(sys.argv[2])
+n_models = int(sys.argv[3])
 
-n_models = 1
-if len(sys.argv) > 3:
-    n_models = int(sys.argv[3])
 
-models = [PPO2.load(sys.argv[1]) for i in range(n_models)]
+def get_model_fps(model_path, secs):
+    model = PPO2.load(model_path)
+    obs = model.observation_space.sample()
+    model.predict(obs, deterministic=True)
 
-def step_all(models):
-    obs = models[0].observation_space.sample()
-    for model in models:
+    i = 0
+    ts = time.time()
+    while time.time() < ts + secs:
+        obs = model.observation_space.sample()
         model.predict(obs, deterministic=True)
+        i += 1
+    return i/secs
 
 
-i = 0
-ts = time.time()
-while time.time() < ts + exec_time:
-    step_all(models)
-    i = i + 1
+# Start tests
+if n_models > 1:
+    args = [(model_path, secs) for i in range(n_models)]
+    pool = Pool(n_models)
+    timings = pool.starmap(get_model_fps, args)
+    fps = np.average(timings)
+else:
+    fps = get_model_fps(model_path, secs)
 
-print("Average FPS: {}".format(i / exec_time / n_models))
+print("Average FPS: {}".format(fps))
